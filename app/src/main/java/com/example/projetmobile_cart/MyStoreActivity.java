34710105223e0d84d66981a5f9fb91ef8a3e6cd2
zakaria <zak69164@gmail.com>
2023-04-16 06:05:50 +0000

    Android project

---
+++ app/src/main/java/com/example/projetmobile_cart/MyStoreActivity.java
@@ -0,0 +1,14 @@
+package com.example.projetmobile_cart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyStoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_store);
+    }
+}
